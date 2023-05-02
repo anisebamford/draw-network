5e06bfacb1530fb135509841f91ea72a25ec1c12
Anise <anisebamford@gmail.com>
2023-05-01 17:51:22 -0700

    Add test for unserialize

--- src/Node.test.ts
+++ src/Node.test.ts
@@ -4,3 +4,8 @@ it("Will create a new node", () => {
   const node = new Node(1, 2)
   expect(node).toBeInstanceOf(Node)
 })
+
+it("Will unserialize", () => {
+  const node = Node.unfreeze(JSON.stringify({x: 1, y: 2}))
+  expect(node).toBeInstanceOf(Node)
+})
