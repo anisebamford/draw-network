fa9d5c836c5a2bb755d92eced6018be6c73e1494
Anise Bamford (they/she) <charliejoybamford@gmail.com>
2023-06-02 14:21:55 -0700

    Add generic node type to edge

--- dist/Edge.d.ts
+++ dist/Edge.d.ts
@@ -1,7 +1,7 @@
 import { Node } from "./Node";
-export declare class Edge {
-    protected myNode1: Node;
-    protected myNode2: Node;
-    get nodes(): [Node, Node];
-    constructor(myNode1: Node, myNode2: Node);
+export declare class Edge<TNode extends Node = Node> {
+    protected myNode1: TNode;
+    protected myNode2: TNode;
+    get nodes(): [TNode, TNode];
+    constructor(myNode1: TNode, myNode2: TNode);
 }
